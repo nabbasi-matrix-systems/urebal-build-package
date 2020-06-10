5fc9e5624332d2165bad1f3f4f4049f86f3c25de
noman-matrix-systems <nabbasi@softpak.com>
2020-06-10 16:00:53 +0500

    Added scripts files

---
+++ release-package/DB-Scripts/incrementals/DropCreateTables.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users(
+    user_id SERIAL,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    is_active BOOLEAN DEFAULT true,
+    PRIMARY KEY (user_id)
+);
+
+DROP TABLE IF EXISTS warehouses;
+
+CREATE TABLE IF NOT EXISTS warehouses (
+    warehouse_id SERIAL,
+    warehouse_name VARCHAR(255),
+    PRIMARY KEY (warehouse_id)
+);
+
+DROP TABLE IF EXISTS companies;
+
+CREATE TABLE IF NOT EXISTS companies (
+    company_id SERIAL,
+    company_name VARCHAR(255),
+    warehouse_id INTEGER ,
+    PRIMARY KEY (company_id),
+    FOREIGN KEY (warehouse_id) REFERENCES warehouses (warehouse_id)
+);
---
+++ release-package/DB-Scripts/incrementals/Views.sql
@@ -0,0 +1,6 @@
+DROP VIEW IF EXISTS WAREHOUSE_COMPANIES;
+
+CREATE VIEW WAREHOUSE_COMPANIES AS
+SELECT w.warehouse_id, w.warehouse_name, c.company_id, c.company_name from warehouses w
+INNER JOIN companies c
+ON w.warehouse_id = c.warehouse_id;
