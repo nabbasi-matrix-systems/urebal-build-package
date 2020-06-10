c3af17d8055ef333306821aae77447a1c6af6d4a
noman-matrix-systems <nabbasi@softpak.com>
2020-06-10 16:35:47 +0500

    Fix scrippt mkdir

--- release-package/DB-Scripts/Incrementals/SetupEnvironment.sh
+++ release-package/DB-Scripts/Incrementals/SetupEnvironment.sh
@@ -1,5 +1,6 @@
 #Version of new release package
 export version=1.0.62
+
 #Script execution logs will be generated on this path. Spaces should be escaped using backslash (\) in outputPath.
 export outputPath=/tmp/
 export host=3.87.65.130
