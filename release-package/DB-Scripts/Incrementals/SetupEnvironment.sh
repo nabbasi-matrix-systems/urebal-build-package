6be126168c3d5b1193618f55802b981f4bb8dd44
noman-matrix-systems <nabbasi@softpak.com>
2020-06-10 16:11:01 +0500

    Added DB Scripts

---
+++ release-package/DB-Scripts/Incrementals/SetupEnvironment.sh
@@ -0,0 +1,9 @@
+#Version of new release package
+export version=1.0.62
+#Script execution logs will be generated on this path. Spaces should be escaped using backslash (\) in outputPath.
+export outputPath=/tmp
+export host=3.87.65.130
+export port=5432
+export databaseName=unsxcgem
+export databaseUser=unsxcgem
+export PASSWORD=zdHCKI_k0wI7UioJbIJCLjmnj7cMMc5R
--- release-package/DB-Scripts/incrementals/SetupEnvironment.sh
+++
@@ -1,9 +0,0 @@
-#Version of new release package
-export version=1.0.62
-#Script execution logs will be generated on this path. Spaces should be escaped using backslash (\) in outputPath.
-export outputPath=/home/urebal/bin
-export host=192.168.2.45
-export port=5432
-export databaseName=UR_DEV
-export databaseUser=UR_DEV
-export PASSWORD=UR_DEV
