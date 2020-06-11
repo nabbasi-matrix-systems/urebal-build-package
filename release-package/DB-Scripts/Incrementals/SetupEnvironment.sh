f18ba9a109f6743bbc88d7c396029e9e2b77e848
Noman Ali Abbasi <66420562+nabbasi-matrix-systems@users.noreply.github.com>
2020-06-11 10:23:40 +0500

    Update SetupEnvironment.sh

--- release-package/DB-Scripts/Incrementals/SetupEnvironment.sh
+++ release-package/DB-Scripts/Incrementals/SetupEnvironment.sh
@@ -1,10 +1,10 @@
 #Version of new release package
-export version=1.0.62
+#export version=1.0.62
 
 #Script execution logs will be generated on this path. Spaces should be escaped using backslash (\) in outputPath.
-export outputPath=/tmp/
-export host=3.87.65.130
-export port=5432
-export databaseName=unsxcgem
-export databaseUser=unsxcgem
-export PASSWORD=zdHCKI_k0wI7UioJbIJCLjmnj7cMMc5R
+#export outputPath=/tmp/
+#export host=3.87.65.130
+#export port=5432
+#export databaseName=unsxcgem
+#export databaseUser=unsxcgem
+#export PASSWORD=zdHCKI_k0wI7UioJbIJCLjmnj7cMMc5R
